4f7c59ec5f539e5f4967c4bd0c0123ed92f55056
Elyor Usmanov <94918957+masterusmanov@users.noreply.github.com>
2023-10-05 10:42:57 +0500

    update

--- src/service/axios.js
+++ src/service/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 // axios.defaults.baseURL = "http://localhost:1987/api"
-axios.defaults.baseURL = "https://https://masterusmanov.onrender.com/api"
+axios.defaults.baseURL = "https://masterusmanov.onrender.com/api/"
 
 export default axios
